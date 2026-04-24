228a12487d6263ac05aba680c46dec311cbc51a2
Andrew McInnis <mcinnisandrewj@gmail.com>
2026-04-24 13:52:28 -0500

    fixed merge conflict in test files

--- src/test/java/cs350/poker/model/CardTest.java
+++ src/test/java/cs350/poker/model/CardTest.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package cs350.poker.model;
 
 import static org.junit.Assert.fail;
@@ -69,75 +69,3 @@ public class CardTest {
 	}
 	
 }
-=======
-package cs350.poker.model;
-
-import static org.junit.Assert.fail;
-//static brings the classes in. as in junit.test vs just .test 
-import org.junit.*;
-
-public class CardTest {
-		
-	private class testCard extends Card{
-		public testCard(int f, int s) {
-			super (f,s);
-		}
-		public int getFace() {
-			return this.face;
-		}
-		public int getSuit() {
-			return this.suit;
-		}
-	}
-	
-	@Test 
-	public void test_constructor() throws Exception {
-		
-		testCard c = new testCard(1,2); 
-		
-		Assert.assertNotNull(c);
-		
-		Assert.assertEquals(1,c.getSuit());
-		Assert.assertEquals(2,c.getFace());
-		
-	}
-	
-	@Test 
-	public void test_getFaceNumLow() throws Exception {
-		
-		testCard c = new testCard(1,2); 
-		
-		Assert.assertEquals(2, c.getFaceNum());
-	}
-	
-	@Test 
-	public void test_getFaceNumHigh() throws Exception {
-		
-		testCard c = new testCard(4,13); 
-		
-		Assert.assertEquals(13, c.getFaceNum());
-	}
-	
-	@Test 
-	public void test_getRankLow() throws Exception {
-		testCard c = new testCard (1, 2);
-		
-		Assert.assertEquals(1, c.getRank());
-	}
-	
-	@Test 
-	public void test_getRankHigh() throws Exception {
-		testCard c = new testCard(4,1);
-		
-		Assert.assertEquals(52, c.getRank());
-	}
-	
-	@Test 
-	public void test_getSuitString() throws Exception {
-		testCard c = new testCard(4,1); 
-		
-		Assert.assertEquals(0, 0);
-	}
-	
-}
->>>>>>> ccc553f0b204f2a80b503faaf84ff2c8d126f734
